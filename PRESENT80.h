7c7d35b72e8d62c2d881b6bd212b1afebc0cfd22
VarunsRepository <57614734+VarunsRepository@users.noreply.github.com>
2026-02-28 17:06:17 +0000

    __m128i pLayer_using_SIMD_Only(__m128i state);
    //SIMD approach to permutate the p-layer. 
    Not working.
    Some correction is still needed.

--- PRESENT80.h
+++ PRESENT80.h
@@ -15,19 +15,20 @@ class PRESENT_80_CORE{
     private:
     __m128i RoundKeys[31];
     static const uint8_t PRESENT80_SBOX[16];  // S-Box for the cypher
+    static const __m128i RC[32]; // an array that holds round constants for al 32 rounds
     __m128i shiftBytesInsideBlock(__m128i block, present80_internal::rotationType mode); // helper function that'll help with the rotation
 
-    __m128i rotateLeftBy61Bits(__m128i block);
-
-    __m128i rotateLeftBy61Bits_SIMD_ONLY(__m128i block);
+    __m128i rotateLeftBy61Bits(__m128i block); // The traditional way to shift 61 bits to the left
+    __m128i rotateLeftBy61Bits_SIMD_ONLY(__m128i block); // The SIMD approach to shify 61 bits to the left by only right shifts
  
     void expandRoundKeys(__m128i key, uint8_t Round);
     //uint8_t applySBox(uint8_t x); 
-    //__m128i sBoxLayer(__m128i state);
-    //__m128i pLayer(__m128i state);
+    __m128i sBoxLayer(__m128i state);
+    __m128i pLayer(__m128i state);   // conventional(scalar) approach to implement p layer
+    __m128i pLayer_using_SIMD_Only(__m128i state); //SIMD approach to permtate the p-layer. not tested yet. Some correction needed
     
     
     public:
     PRESENT_80_CORE(__m128i key);
-    //__m128i encrypt64();
+    __m128i encrypt64(uint64_t  block);
 };
